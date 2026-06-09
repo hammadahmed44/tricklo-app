f31476396dc95ddbba3f312b5401dcc3c3498cb7
Hammad Ahmed <Hammad.Ahmed@dataqhealth.com>
2026-06-09 17:04:04 +0500

    first commit from office

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const start = async () => {
   await connectDB();
 
   server.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+    console.log(`Server running on ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
   });
 };
 
