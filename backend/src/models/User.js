5322cd0d360fea660de67773ceaf86d59826b0e0
Hammad Ahmed <Hammad.Ahmed@dataqhealth.com>
2026-06-11 19:40:52 +0500

    second days user modal creation

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,12 +1,15 @@
 // TODO Day 2 — YOU write the schema (study: bcrypt, JWT, Mongoose methods)
 // After writing, ask AI to review it
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
 // Fields to implement:
 // name, email (unique), password (bcrypt hashed), avatar (Cloudinary URL),
 // refreshToken (hashed in DB), isVerified (Boolean), resetPasswordToken, resetPasswordExpires, createdAt
 
+
+ 
 // Methods to add on schema:
 //   schema.methods.comparePassword = async function(plainText) { ... bcrypt.compare }
 //   schema.methods.generateAccessToken = function() { ... jwt.sign }
@@ -17,7 +20,29 @@ const mongoose = require('mongoose');
 // to generate new access tokens. Same reason we hash passwords.
 
 const userSchema = new mongoose.Schema({
-  // implement me
-});
+ name:{type:String,required:true,trim:true},
+  email:{tyep:String,required:true,unique:true,lowercase:true,trim:true},
+  password:{type:String,required:true,miminlength: 6, select: false },
+  avatar:{type:String,default:''},
+    refreshToken:{type:String,default:null},
+  isverified:{type:Boolean,default:false},
+  resetPasswordToken: { type: String, default: null },
+    resetPasswordExpires: { type: Date, default: null },
+    
+},  { timestamps: true }
+);
+userSchema.methods.comparePassword=async function (plainText) {
+  return bcrypt.compare(plainText,this.password)
+}
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_ACCESS_SECRET, {
+    expiresIn: process.env.JWT_ACCESS_EXPIRES || '15m',
+  });
+};
 
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: process.env.JWT_REFRESH_EXPIRES || '7d',
+  });
+};
 module.exports = mongoose.model('User', userSchema);
